cd0106e20d70ebb282f7d49d16fe413a0b9e66b3
Rutger Speksnijder <rutgerspeksnijder@hotmail.com>
2016-07-19 09:14:57 +0200

    Updated router to use "/" as a default route

--- src/SimpleRoute/Router.php
+++ src/SimpleRoute/Router.php
@@ -1,4 +1,27 @@
 <?php
+/*
+    The MIT License (MIT)
+
+    Copyright (c) 2016 Rutger Speksnijder
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
 namespace SimpleRoute;
 
 /**
@@ -275,11 +298,12 @@ class Router {
     public function execute() {
         // Check if we have a url
         if (!$this->url || trim($this->url) == '') {
-            $this->url = '';
-        } else {
-            if (substr($this->url, 0, 1) !== '/') {
-                $this->url = '/' . $this->url;
-            }
+            $this->url = '/';
+        }
+
+        // Make sure the url starts with a slash
+        if (substr($this->url, 0, 1) !== '/') {
+            $this->url = '/' . $this->url;
         }
 
         // Check if the absolute route exists in our routes array
@@ -314,12 +338,12 @@ class Router {
 
         // No route found, check if we have an empty route.
         // - We should always have an empty route.
-        if (!isset($this->routes['any'][''])) {
+        if (!isset($this->routes['any']['/'])) {
             // No empty route found, throw an exception
             throw new \Exception("No \"not found\" route set.");
         }
 
         // Return the empty route's callable
-        return $this->routes['any']['']();
+        return $this->routes['any']['/']();
     }
 }
